a6cf53adc8d24028a795b2ae97c1c6b0ba64109b
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-06-27 18:10:57 -0400

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,11 +10,14 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+  });
 }
 // Add routes, both API and view
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/listings", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds161446.mlab.com:61446/heroku_9k3dk1wf", {
 });
 
 app.listen(PORT, function() {
